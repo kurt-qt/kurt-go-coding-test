ca3263e378ea533bcab3a8098f7ab207355708c3
KurtPatrickGo <kurtpatrick@qualitytrade.com>
2022-10-28 12:37:27 +0800

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('products/', [ProductController::class, 'index']);
+Route::get('products/{product}', [ProductController::class, 'show']);
+Route::post('products/', [ProductController::class, 'create']);
